1973ba4a6f5ee26f4f5f28e76231ea75a500f2a9
eneun <wtcat@naver.com>
2019-04-24 17:42:15 +0900

    model create

--- crud/models.py
+++ crud/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add = True) # 만든 날짜, 시각 자동 저장
+
+    def __str__(self):
+        return self.title
+    
+    def summary(self):
+        return self.content[:20]
